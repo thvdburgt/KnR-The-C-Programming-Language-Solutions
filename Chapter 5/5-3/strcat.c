60d3d33c16335daab05266c023cfdba28bf68710
Thomas van der Burgt <thomas@thvdburgt.nl>
2010-03-04 19:36:11 +0100

    Solution to 5-3

---
+++ Chapter 5/5-3/strcat.c
@@ -0,0 +1,35 @@
+/*
+ * Filename:    strcat.c
+ * Author:      Thomas van der Burgt <thomas@thvdburgt.nl>
+ * Date:        03-MAR-2010
+ *
+ * The C Programming Language, second edition,
+ * by Brian Kernighan and Dennis Ritchie
+ *
+ * Exercise 5-3, page 107
+ *
+ * Write a pointer version of the function strcat that we showed in
+ * Chapter 2: strcat(s,t) copies the string t to the end of s.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/* strcat:  concatenate t to end of s; s must be big enough */
+void strcat(char *s, const char *t)
+{
+    while (*s)       /* find end of s */
+        s++;
+    do               /* copy t */
+        *s = *t++;
+    while (*s++);
+}
+
+int main(void)
+{
+    char s[100] = "ab\0c";
+    char t[100] = "def";
+    strcat(s, t);
+    puts(s);
+    return EXIT_SUCCESS;
+}
