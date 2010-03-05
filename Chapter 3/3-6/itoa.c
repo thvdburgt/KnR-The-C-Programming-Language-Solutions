fe2ef2fd9722d7c9af6baef2b3fc72b326147d18
Thomas van der Burgt <thomas@thvdburgt.nl>
2010-03-05 11:06:48 +0100

    correct comment indenting

--- Chapter 3/3-6/itoa.c
+++ Chapter 3/3-6/itoa.c
@@ -43,8 +43,8 @@ void itoa(int n, char s[], int w)
     sign = n;
     i = 0;
     do {       /* generate digits in reverse order */
-        s[i++] = abs(n % 10) + '0';   /* get next digit */
-    } while (n /= 10);               /* delete it */
+        s[i++] = abs(n % 10) + '0';     /* get next digit */
+    } while (n /= 10);                  /* delete it */
 
     if (sign < 0)
         s[i++] = '-';
