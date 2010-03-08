5fcc99da313caea68d82b6e6bd0f42378eee9785
Thomas van der Burgt <thomas@thvdburgt.nl>
2010-03-08 21:40:25 +0100

    fix solution for 4-2

--- Chapter 4/4-2/atof.c
+++ Chapter 4/4-2/atof.c
@@ -1,4 +1,20 @@
-#include <math.h>
+/*
+ * Filename:    atof.c
+ * Author:      Thomas van der Burgt <thomas@thvdburgt.nl>
+ * Date:        08-MAR-2010
+ *
+ * The C Programming Language, second edition,
+ * by Brian Kernighan and Dennis Ritchie
+ *
+ * Exercise 4-2, page 73
+ *
+ * Extend atof to handle scientific notation of the form 123.45e-6 
+ * where a floating-point number may be followed by e or E and an
+ * optionally signed exponent.
+ */
+
+#include <ctype.h>  /* isspace() & isdigit() */
+#include <math.h>   /* pow() */
 
 /* atof:  convert string s to double */
 float atof(char s[])
@@ -11,22 +27,24 @@ float atof(char s[])
     sign = (s[i] =='-') ? -1 : 1;
     if (s[i] == '+' || s[i] == '-')
         i++;
-    for (val = 0.0; isdigit(s[i]); i++)
+    for (val = 0.0; isdigit(s[i]); i++)     /* integer */
         val = 10.0 * val + (s[i] - '0');
-    if (s[i] == '.')
+    if (s[i] == '.') {                      /* fraction */
         i++;
-    for (power = 1.0; isdigit(s[i]); i++) {
-        val = 10.0 * val + (s[i] - '0');
-        power *= 10.0;
+        for (power = 1.0; isdigit(s[i]); i++) {
+            val = 10.0 * val + (s[i] - '0');
+            power *= 10.0;
+        }
+        val *= sign / power;
     }
-    if (s[i] == 'e' || s[i] == 'E')
-        i++;
-    esign = (s[i] =='-') ? -1 : 1;
-    if (s[i] == '+' || s[i] == '-') /* FIXME: don't accept 123+456 !!!! */
-        i++;
-    for (exp = 0.0; isdigit(s[i]); i++)
-        exp = 10.0 * exp + (s[i] - '0');
-    
-    return (sign * val / power) * pow(10, esign * exp);
+    if (s[i] == 'e' || s[i] == 'E') {       /* exponent*/
+        esign = (s[++i] == '-') ? -1 : 1;
+        if (s[i] == '+' || s[i] == '-')
+            i++;
+        for (exp = 0.0; isdigit(s[i]); i++)
+            exp = 10.0 * exp + (s[i] - '0');
+        val *= pow(10, esign * exp);
+    }
+    return val;
 }
 
